8d00c379b89307e5735ca8b5980d0be87838b907
tomerzd4444 <50952695+tomerzd4444@users.noreply.github.com>
2020-09-14 10:03:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import discord
 from discord.ext import commands
+from discord.utils import get
 from dotenv import load_dotenv
 from utils import BotUtils
 
@@ -12,6 +13,11 @@ utils = BotUtils(bot)
 print(token)
 killChannel = None
 
+
+@bot.event
+async def on_member_join(member: discord.Member):
+		role = get(member.server.roles, id=705761329901994015)
+		await bot.add_roles(member, role)
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
@@ -34,5 +40,14 @@ async def kill(ctx: commands.context.Context, user: discord.Member):
     except commands.errors.CommandInvokeError:
         await ctx.send(f"the user {user.display_name} is not connected to voice chat u stupid fuck")
 
+
+@bot.command()
+async def ban(ctx: commands.context.Context, user: discord.Member):
+	if user.id == 306114855063257099:
+		await ctx.send("u can not ban that user u fuck")
+		await ctx.guild.ban(ctx.message.author, reason="you tried to ban blueblood u fuck")
+		return
+	await ctx.guild.ban(user,reason="fuck u")
+	await ctx.send(f"banned the user {user.display_name} cause fuck u")
 
 bot.run(token)
