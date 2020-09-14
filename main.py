459b14bd207ea99027152190ca9a993029bf4b76
Tomer Zelkha Dinur <tomerzd4444@gmail.com>
2020-09-14 18:57:33 +0300

    Added the addrole and removerole commands

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ killChannel = None
 
 
 @bot.event
-async def on_member_join(member: discord.Member):
-		role = get(member.server.roles, id=705761329901994015)
-		await bot.add_roles(member, role)
-@bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
@@ -43,11 +39,24 @@ async def kill(ctx: commands.context.Context, user: discord.Member):
 
 @bot.command()
 async def ban(ctx: commands.context.Context, user: discord.Member):
-	if user.id == 306114855063257099:
-		await ctx.send("u can not ban that user u fuck")
-		await ctx.guild.ban(ctx.message.author, reason="you tried to ban blueblood u fuck")
-		return
-	await ctx.guild.ban(user,reason="fuck u")
-	await ctx.send(f"banned the user {user.display_name} cause fuck u")
+    if user.id == 306114855063257099:
+        await ctx.send("u can not ban that user u fuck")
+        await ctx.guild.ban(ctx.message.author, reason="you tried to ban blueblood u fuck")
+        return
+    await ctx.guild.ban(user, reason="fuck u")
+    await ctx.send(f"banned the user {user.display_name} cause fuck u")
 
+
+@bot.command()
+async def addrole(ctx: commands.context.Context, role: discord.role.Role, user: discord.Member = None):
+    if user is None:
+        user = ctx.author
+    print(user, role)
+    await user.add_roles(role)
+
+
+@bot.command()
+@commands.has_role("developer")
+async def removerole(ctx: commands.context.Context, user: discord.member.Member, role: discord.role.Role):
+    await user.remove_roles(role)
 bot.run(token)
